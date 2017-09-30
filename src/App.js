177b149064891e400c2f7411868b7d01fdd229df
Johannes Camin <johannes.camin@giga-infosystems.com>
2017-10-01 00:27:36 +0200

    demo that requests one country from new graphql api

--- src/App.js
+++ src/App.js
@@ -5,13 +5,34 @@ import "./App.css";
 
 const myQuery = gql`
   {
-    hello
+    byCountry(country: "Germany") {
+      translations {
+        name
+        language
+        translation
+      }
+    }
   }
 `;
 
 class App extends Component {
   render() {
-    return <h1>{this.props.data.hello}</h1>;
+    console.log(this.props);
+    const content = this.props.data.byCountry || { translations: [] };
+    const translations = content.translations.map(t => (
+      <li key={t.language}>
+        {t.language} - {t.translation}
+      </li>
+    ));
+    const country =
+      content.translations.length > 0 ? content.translations[0].name : "";
+    console.log(content);
+    return (
+      <div>
+        <h4>{country}</h4>
+        <ul>{translations}</ul>
+      </div>
+    );
   }
 }
 
